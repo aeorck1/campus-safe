a6daf2c95e9e3b354889017543c420c56f1049ae
Godwin Daniel <lemuelgodwin2@gmail.com>
2025-07-05 16:13:39 +0100

    Changed the baseURL

--- lib/axiosAuth.ts
+++ lib/axiosAuth.ts
@@ -4,8 +4,8 @@ import { useAuthStore } from './auth';
 
 const axiosAuth = axios.create({
   // baseURL: 'http://192.168.165.62:8000/api/v1/',
-  baseURL: 'http://127.0.0.1:8000/api/v1/',
-  // baseURL: 'https://campussecuritybackend.onrender.com/api/v1/',
+  // baseURL: 'http://127.0.0.1:8000/api/v1/',
+  baseURL: 'https://campussecuritybackend.onrender.com/api/v1/',
   headers: {
     'Content-Type': 'application/json',
   },
