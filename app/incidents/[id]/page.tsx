0227b46e6a8404fa05774f64225d1c0c8bff6dca
Godwin Daniel <lemuelgodwin2@gmail.com>
2025-07-26 03:50:44 +0100

    Refactor IncidentPage to use async params destructuring for improved readability

--- app/incidents/[id]/page.tsx
+++ app/incidents/[id]/page.tsx
@@ -2,11 +2,12 @@ import { Suspense } from "react"
 import { IncidentDetails } from "@/components/incidents/incident-details"
 import { IncidentDetailsSkeleton } from "@/components/skeletons/incident-details-skeleton"
 
-export default function IncidentPage({ params }: { params: { id: string } }) {
+export default async function IncidentPage({ params }: { params: { id: string } }) {
+   const {id}= await params
   return (
     <div className="space-y-6 border-b pb-6 m-auto max-w-full md:w-[90%] w-[95%] my-[20px]">
       <Suspense fallback={<IncidentDetailsSkeleton />}>
-        <IncidentDetails id={params.id} />
+        <IncidentDetails id={id} />
       </Suspense>
     </div>
   )
