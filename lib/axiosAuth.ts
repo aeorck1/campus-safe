370d686ccd7e4b6980c8201ff89899b46d9fb040
Godwin Daniel <61164722+aeorck1@users.noreply.github.com>
2025-09-27 19:01:15 +0100

    Switch baseURL for axiosAuth to local development

--- lib/axiosAuth.ts
+++ lib/axiosAuth.ts
@@ -4,8 +4,8 @@ import { useAuthStore } from './auth';
 
 const axiosAuth = axios.create({
   // baseURL: 'http://192.168.73.62:8000/api/v1/',
-  // baseURL: 'http://127.0.0.1:8000/api/v1/',
-  baseURL: 'https://campussecuritybackend-mjlf.onrender.com/api/v1/',
+  baseURL: 'http://127.0.0.1:8000/api/v1/',
+  // baseURL: 'https://campussecuritybackend-mjlf.onrender.com/api/v1/',
   headers: {
     'Content-Type': 'application/json',
   },
